ac76a6deb58c8f7dfcae02cbf4dbcbf21fa5691a
liyuanchao <liyuanchao@meituan.com>
2017-05-02 18:27:40 +0800

    添加工程

---
+++ SubmoduleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubmoduleDemo
+//
+//  Created by yuanchao Li on 2017/5/2.
+//  Copyright © 2017年 yuanchao Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SubmoduleDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SubmoduleDemo
+//
+//  Created by yuanchao Li on 2017/5/2.
+//  Copyright © 2017年 yuanchao Li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
