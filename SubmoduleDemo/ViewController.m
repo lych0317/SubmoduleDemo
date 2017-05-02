f946ebb1782d9cb2c4e6562af7cc840baf10c89b
liyuanchao <liyuanchao@meituan.com>
2017-05-02 18:52:16 +0800

    添加submodule

--- SubmoduleDemo/ViewController.m
+++ SubmoduleDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "StaticLibForSubmodule.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    StaticLibForSubmodule *module = [[StaticLibForSubmodule alloc] init];
+    module.name = @"af";
+    NSLog(@"name %@", module.name);
 }
 
 
